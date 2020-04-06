337d1fc513803590a8a65feac685af4392301306
vomehad <vomehad.yandex>
2020-04-06 04:11:25 +0300

    v0.17

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ let generated = [];
 initButtons(); // "берём" кнопки
 
 create.onclick = () => { // кнопка создать
-    let inputValues = document.getElementsByTagName('input');
     let div = document.getElementById('result');
-    recreateArray(div);
+    let inputValues = document.getElementsByTagName('input');
+    recreateArray(div); // очищаем место для нового массива
 
     min = demoMin(inputValues); // исключаем некорректные значения (демо-режим)
     max = demoMax(inputValues, min); // исключаем некорректные значения (демо-режим)
     generated = generateArray(getSourceArray(min, max)); // "перемешаный" массив
 
-    elemInDiv(div);
+    elemInDiv(div); // выведем полученный массив
 }
 
 bubbleSort.onclick = () => { // кнопка пузырьковая сортировка
-    checkQueue();
+    checkQueue(); // проверка на существование массива
     let elems = generated.length; // колличество элементов в исходном массве
     let needSort = true; // флаг для проходов сортировки
     let div = document.getElementById('result'); // ход сортировки
 
     while (needSort) { // непосредственно сортировка
         let ul = document.createElement('ul'); // проход сортировки
-        ul.className = "sort-iter"; // класс для css
+        ul.className = "sort-iter";
 
         needSort = false; // флаг для проходов сортировки
         for (let i = 0; i < (elems - 1); i++) { // этапы прохода
             if (generated[i] > generated[i + 1]) { // нужно ли переставить элементы?
-                needSort = true;
-                sorting(ul, i, needSort);
+                needSort = true; // флаг для повторного прохода
+                sorting(ul, i, needSort); // перестановка и вывод этапа
             } else {
-                sorting(ul, i);
+                sorting(ul, i); // вывод этапа
             }
             div.append(ul); // конец прохода
         }
@@ -41,21 +41,21 @@ bubbleSort.onclick = () => { // кнопка пузырьковая сортир
     let end = document.createElement('div'); // вывод об окончании сортировки
     end.className = "sorted"; // класс для css
 
-    elemInDiv(div);
+    elemInDiv(div); // вывод отсортированного массива
 }
 
 function sorting(ul, i, need = false) {
-    let li = document.createElement('li'); // вывод этапа
+    let li = document.createElement('li');
     if (need) {
-        reshuffle(generated, i); // непосредственно перестановка
+        reshuffle(generated, i);
 
         li.className = "shuffling";
         li.innerHTML = generated[i] + ' > ' + generated[i + 1];
     } else {
-        li.className = "sort-yet"; // класс для css
+        li.className = "sort-yet";
         li.innerHTML = generated[i] + ' < ' + generated[i + 1];
     }
-    ul.append(li); // вставим этап в проход
+    ul.append(li);
 }
 
 clear.onclick = () => {
@@ -74,24 +74,10 @@ function initButtons() {
     clear = buttons[2];
 }
 
-function elemInDiv(div) {
-    let create = document.createElement('div');
-    create.className = "create";
-    if (generated.length > 0) {
-        Array.from(generated).forEach((el) => {
-            let elem = document.createElement('div');
-            elem.className = "block-array";
-            elem.innerHTML = el;
-            create.appendChild(elem);
-        });
+function recreateArray(result) {
+    while (result.firstChild) {
+        result.removeChild(result.firstChild);
     }
-    div.append(create);
-}
-
-function reshuffle(array, base) {
-    [array[base], array[base + 1]] = [array[base + 1], array[base]];
-
-    return array;
 }
 
 function getSourceArray(min, max) {
@@ -110,6 +96,32 @@ function generateArray(source) {
         source.splice(index, 1);
     }
     return gener;
+}
+
+function elemInDiv(div) {
+    let create = document.createElement('div');
+    create.className = "create";
+    if (generated.length > 0) {
+        Array.from(generated).forEach((el) => {
+            let elem = document.createElement('div');
+            elem.className = "block-array";
+            elem.innerHTML = el;
+            create.appendChild(elem);
+        });
+    }
+    div.append(create);
+}
+
+function checkQueue() {
+    if (generated.length == 0) {
+        return alert('сначала создайте массив');
+    }
+}
+
+function reshuffle(array, base) {
+    [array[base], array[base + 1]] = [array[base + 1], array[base]];
+
+    return array;
 }
 
 function demoValue() {
@@ -133,16 +145,4 @@ function demoMax(input, min = 0) {
         max = min + DEMO_LENGTH;
     }
     return max;
-}
-
-function recreateArray(result) {
-    while (result.firstChild) {
-        result.removeChild(result.firstChild);
-    }
-}
-
-function checkQueue() {
-    if (generated.length == 0) { // если забыли создать исходный массив
-        return alert('сначала создайте массив'); // создайте его
-    }
 }
